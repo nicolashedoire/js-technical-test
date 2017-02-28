2c501788cef7fb4b0a8eae91dc5f5dafccf91ac8
Hedoire <codaj@support.codaj.com>
2017-02-28 09:33:39 +0100

    add -> get dynamically url https://api.github.com/repos/:owner/:repo/issues/:number

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,34 @@
 // create angular app
 var app = angular.module('app' , []);
 
-app.controller('mainController', function(){
+app.controller('mainController', function($scope , $http){
+
+	// default values
+	$scope.owner  = 'nodejs';
+	$scope.repo   = 'node';
+	$scope.number = '6867'; 
+
+	// link api github https://developer.github.com/v3/issues/
+	$http.get('https://api.github.com/repos/nodejs/node/issues/6867').then(function(res){
+    	$scope.title          = res.data.title;
+    	$scope.user           = res.data.user.login;
+    	$scope.avatar         = res.data.user.avatar_url;
+    	$scope.message        = res.data.body;
+    });
+
+
+    $scope.getIssue = function(){
+    	
+    	$scope.url = 'https://api.github.com/repos/' + $scope.owner + '/' + $scope.repo + '/issues/' + $scope.number;
+    	console.log($scope.url);
+
+		$http.get($scope.url).then(function(res){
+	    	$scope.title          = res.data.title;
+	    	$scope.user           = res.data.user.login;
+	    	$scope.avatar         = res.data.user.avatar_url;
+	    	$scope.message        = res.data.body;
+	    });
+
+    }
 	
 });
