9aa275079fc6092c34f210eef102db1179bcc4e7
Hedoire <codaj@support.codaj.com>
2017-02-28 09:37:48 +0100

    add -> set dynamic url

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,14 @@
 // create angular app
 var app = angular.module('app' , []);
 
-app.controller('mainController', function($scope , $http){
+app.controller('mainController', function($scope , $http , $location){
 
 	// default values
 	$scope.owner  = 'nodejs';
 	$scope.repo   = 'node';
 	$scope.number = '6867'; 
+
+	$location.url($scope.owner + '/' + $scope.repo + '/' + $scope.number);
 
 	// link api github https://developer.github.com/v3/issues/
 	$http.get('https://api.github.com/repos/nodejs/node/issues/6867').then(function(res){
@@ -18,15 +20,14 @@ app.controller('mainController', function($scope , $http){
 
 
     $scope.getIssue = function(){
-    	
+    	$location.url($scope.owner + '/' + $scope.repo + '/' + $scope.number);
     	$scope.url = 'https://api.github.com/repos/' + $scope.owner + '/' + $scope.repo + '/issues/' + $scope.number;
-    	console.log($scope.url);
-
 		$http.get($scope.url).then(function(res){
 	    	$scope.title          = res.data.title;
 	    	$scope.user           = res.data.user.login;
 	    	$scope.avatar         = res.data.user.avatar_url;
 	    	$scope.message        = res.data.body;
+	    	
 	    });
 
     }
